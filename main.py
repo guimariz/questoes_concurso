2e1cf4a6b94746c3092e3644a37ff1a1446e4ca8
guimariz <--reset-author>
2024-08-20 14:23:53 -0300

    pegar o nome dos arquivos pdf, onde no nome deve aver ae ou ce

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from utils.extrair_pdf import *
 from utils.questoes import *
 from utils.adicionar_prova_mongo import *
 
-caminho_pdf = './data/provas/cebraspe-cti-especifica.pdf'
+nome_prova = 'fgv-analista-segurança-ti-tjms'
+nome_banca = nome_prova.split('-')[0]
+caminho_pdf = f'./data/provas/{nome_prova}.pdf'
 
 texto = extrair_texto_do_pdf(caminho_pdf)
 
-questoes = setar_questoes("cebraspe-cti-especifica", texto)
+# questoes = setar_questoes_ce("cebraspe-cti-especifica", texto)
+questoes = setar_questoes_ae(nome_prova, nome_banca, texto)
 
+# print(questoes)
 
-
-
-
-
-# criar_banco('provas_concursos_ti', 'cebraspe-cti-especifica', questoes)
+criar_banco('provas_concursos_ti', nome_prova, questoes)
 
--- utils/questoes.py
+++ utils/questoes.py
@@ -1,6 +1,6 @@
 import re
 
-def setar_questoes(nome, texto):
+def setar_questoes_ce(nome, nome_banca, texto):
     padrao = r"(\d+)\s([^\n]+(?:\n[^\d]+)*)"
 
     questoes = re.findall(padrao, texto)
@@ -8,7 +8,29 @@ def setar_questoes(nome, texto):
 
     for questao in questoes:
         numero = questao[0]
-        enunciado = ' '.join(questao[1].split())
-        questoes_dic[numero] = enunciado
+        if int(numero) >= 51:
+            enunciado = ' '.join(questao[1].split())
+            questoes_dic[numero] = enunciado
 
-    return {'nome': nome, 'questoes': questoes_dic}
+    return {'nome': nome, 'nome_banca': nome_banca, 'questoes': questoes_dic}
+
+def setar_questoes_ae(nome, nome_banca, texto):
+    padrao = r"(?P<numero>\d+)\s*\n(?P<enunciado>.+?(?=\s*\n\([ABCDE]\)))\s*\n\(A\)\s*(?P<alternativaA>.+?)\s*\n\(B\)\s*(?P<alternativaB>.+?)\s*\n\(C\)\s*(?P<alternativaC>.+?)\s*\n\(D\)\s*(?P<alternativaD>.+?)\s*\n\(E\)\s*(?P<alternativaE>.+?)"
+
+    questoes = re.finditer(padrao, texto, re.DOTALL)
+    questoes_dic = {}
+
+    for questao in questoes:
+        numero = questao.group("numero")
+        enunciado = ' '.join(questao.group("enunciado").split())
+        alternativas = {
+            "A": ' '.join(questao.group("alternativaA").split()),
+            "B": ' '.join(questao.group("alternativaB").split()),
+            "C": ' '.join(questao.group("alternativaC").split()),
+            "D": ' '.join(questao.group("alternativaD").split()),
+            "E": ' '.join(questao.group("alternativaE").split())
+        }
+        questoes_dic[numero] = {"enunciado": enunciado, "alternativas": alternativas}
+
+    return {'nome': nome, 'nome_banca': nome_banca, 'questoes': questoes_dic}
+
